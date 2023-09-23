90142df433d8ec086ac59e5eaf3707c2d7e8040b
S-J-K123 <98359890+S-J-K123@users.noreply.github.com>
2023-09-23 18:13:11 +0100

    audio

---
+++ src/components/Controls.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Controls = () => {
+    return (
+        <div>
+            controls
+        </div>
+    );
+}
+
+export default Controls;
---
+++ src/data/tracks.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const tracks = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default tracks;
